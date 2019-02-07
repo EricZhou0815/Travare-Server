e3bacb411ff91ea4d68b597f3d0933858370097d
Junchao Zhong <jczhong115@gmail.com>
2019-02-07 23:32:10 +1300

    - tuned user register, login and get profile api

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ module.exports=(req,res,next)=>{
     //token is at the second part of the header.
     //a header is like 'Bearer sfdsj43dfgnheijfoawvns'
     if(req.headers.authorization){
-        const token=req.headers.authorization.split(' ')[1];
+        const token=req.headers.authorization;
         jwt.verify(token,process.env.SECRET,(err,decoded)=>{
             if(err){
                 next(Error('Failed to authentication token'));
--- models/user.js
+++ models/user.js
@@ -1,72 +1,79 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const uniqueValidator = require('mongoose-unique-validator');
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true, 
+const userSchema = new mongoose.Schema({
+    uid: {
+        type: String,
+        index: true,
+        unique: true,
     },
-    password:{
-        type:String,
+    username: {
+        type: String,
+        required: true,
     },
-    thirdPartyAuth:String,
-    firstName:{
-        type:String,
+    password: {
+        type: String,
     },
-    lastName:{
-        type:String,
+    thirdPartyToken: String,
+    thirdPartySource: String,
+    email: String,
+    firstName: {
+        type: String,
     },
-    city:{
-        type:String,
+    lastName: {
+        type: String,
     },
-    country:{
-        type:String,
+    city: {
+        type: String,
     },
-    picture:String,
-    about:{
-        type:String,
+    country: {
+        type: String,
     },
-    mobile:{
-        type:Number,
+    avatar: String,
+    about: {
+        type: String,
     },
-    stars:{
-        type:Number,
-        default:0
+    mobile: {
+        type: Number,
     },
-    greenLevel:{
-        type:Number,
-        default:1
+    stars: {
+        type: Number,
+        default: 0
     },
-    created:{
-        type:Date,
-        default:Date.now,
+    co2Reduction: {
+        type: Number,
+        default: 0
+    },
+    created: {
+        type: Date,
+        default: Date.now,
     }
 });
 
+userSchema.plugin(uniqueValidator);
 
 //encrypt password
-userSchema.pre('save', async function(next){
-    try{
-        if(!this.isModified('password')){
+userSchema.pre('save', async function (next) {
+    try {
+        if (!this.isModified('password')) {
             return next();
         }
-        const hashed=await bcrypt.hash(this.password,12);
-        this.password=hashed;
+        const hashed = await bcrypt.hash(this.password, 12);
+        this.password = hashed;
         return next();
-    } catch(err){
+    } catch (err) {
         return next(err);
     }
 });
 
 //compare password
-userSchema.methods.comparePassword=async function(attempt,next){
-    try{
-        return await bcrypt.compare(attempt,this.password);
-    }catch(err){
+userSchema.methods.comparePassword = async function (attempt, next) {
+    try {
+        return await bcrypt.compare(attempt, this.password);
+    } catch (err) {
         next(err);
     }
-
 }
 
-
-module.exports=mongoose.model('User',userSchema);
+module.exports = mongoose.model('User', userSchema);
